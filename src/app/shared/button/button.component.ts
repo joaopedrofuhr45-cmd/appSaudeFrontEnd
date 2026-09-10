9dd570f7a0e08edb50233a038ca5c8197e68475b
joaopedrofuhr45-cmd <joaopedrofuhr45@gmail.com>
2026-09-09 23:58:18 -0300

    criando button reutilizavel e componente da tela inicial

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() variante: 'primario' | 'perigo' | 'secundario' | 'sucesso' = 'primario';
+  @Input() type: 'button' | 'submit' = 'button';
+  @Input() disabled = false;
+}
