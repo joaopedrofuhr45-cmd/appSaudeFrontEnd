b0d470340b5c3440cc36d02cdc374fa017b07632
NOME <EMAIL>
2026-09-15 09:31:11 -0300

    tela de cadastro

--- src/app/service/cadastro/cadastro.service.ts
+++ src/app/service/cadastro/cadastro.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CadastroRequest } from '../../model/cadastro/cadastro-request';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CadastroService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  private apiUrl = 'http://localhost:8080/api';
+
+
+  cadastro(cadastroCredentials: CadastroRequest){
+    return this.http.post<void>(`${this.apiUrl}/cadastro`,cadastroCredentials, {
+      withCredentials: true,
+    });
+  }
+
 }
